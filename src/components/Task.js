15653470aa1fc7ee1f8fe4f23ddaf5016e197588
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-30 13:01:46 -0800

    added strikethrough on complete to Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
-
+  const [complete, toggleComplete] = useState(isComplete);
+  const onCompleted = (event) => {
+    toggleComplete(!complete);
+  };
+  const buttonClass = complete ? '--completed' : '';
   return (
-    <li className="tasks__item">
+    <li key={id} className="tasks__item">
       <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        className={`tasks__item__toggle${buttonClass}`}
+        onClick={onCompleted}
       >
         {title}
       </button>
